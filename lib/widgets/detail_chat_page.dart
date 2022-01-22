b0f54f14dbc21d5b9817bc6b43a6e4e177304419
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-01-22 07:05:03 +0700

    chat bubble dan detail chat page

--- lib/widgets/detail_chat_page.dart
+++ lib/widgets/detail_chat_page.dart
@@ -2,56 +2,193 @@
 
 import 'package:flutter/material.dart';
 import 'package:shamo_front_end/theme.dart';
+import 'package:shamo_front_end/widgets/chat_bubble.dart';
 
 class DetailChatPage extends StatelessWidget {
   const DetailChatPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    PreferredSize header() {
+      return PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          backgroundColor: backgroundColor1,
+          centerTitle: false,
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/image_shop_logo_online.png',
+                width: 50,
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Shoe Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: medium,
+                    ),
+                  ),
+                  Text(
+                    'Online',
+                    style: secondaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: light,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget productPreview() {
+      return Container(
+        height: 74,
+        width: 225,
+        margin: EdgeInsets.only(
+          bottom: 20,
+        ),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColor5,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: primaryColor,
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'COURT VISION ada pppppppp',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: reguler,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$56,33',
+                    style: priceTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/button_close.png',
+              width: 22,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: backgroundColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        style: primaryTextStyle,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message...',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 45,
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        children: [
+          ChatBubble(
+            isSender: true,
+            text: 'Hi, This item is still available?',
+            hasProduct: true,
+          ),
+          ChatBubble(
+            isSender: false,
+            text: 'Good night, This item is only available in size 42 and 43',
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
-    );
-  }
-
-  PreferredSize header() {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(70),
-      child: AppBar(
-        backgroundColor: backgroundColor1,
-        centerTitle: false,
-        title: Row(
-          children: [
-            Image.asset(
-              'assets/image_shop_logo_online.png',
-              width: 50,
-            ),
-            SizedBox(
-              width: 12,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Shoe Store',
-                  style: primaryTextStyle.copyWith(
-                    fontSize: 14,
-                    fontWeight: medium,
-                  ),
-                ),
-                Text(
-                  'Online',
-                  style: secondaryTextStyle.copyWith(
-                    fontSize: 14,
-                    fontWeight: light,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+      bottomNavigationBar: chatInput(),
+      body: content(),
     );
   }
 }
